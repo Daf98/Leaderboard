6b183bab0d185dbd9518dcd5cca2f0172f614db3
Daf <dafne.azzolina@gmail.com>
2022-06-15 21:23:23 -0300

    Fix indentation errors

--- src/index.js
+++ src/index.js
@@ -8,30 +8,30 @@ const submitButton = document.getElementById('submit');
 
 // send data to the API
 const sendData = async () => {
-    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ai5Nvjw0VJjtGbkLNuyF/scores', {
-        method: 'POST',
-        body: JSON.stringify({
-            user: user.value,
-            score: score.value,
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    });
-    const success = await response.json();
-    return success;
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ai5Nvjw0VJjtGbkLNuyF/scores', {
+    method: 'POST',
+    body: JSON.stringify({
+      user: user.value,
+      score: score.value,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const success = await response.json();
+  return success;
 };
 submitButton.addEventListener('click', sendData);
 // function to receive data and parse JSON
 const receiveData = async () => {
-    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ai5Nvjw0VJjtGbkLNuyF/scores', {
-        method: 'GET',
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    });
-    const gamerArray = await response.json();
-    dynamicScore(user.value, score.value);
-    return gamerArray;
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ai5Nvjw0VJjtGbkLNuyF/scores', {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const gamerArray = await response.json();
+  dynamicScore(user.value, score.value);
+  return gamerArray;
 };
 refreshButton.addEventListener('click', receiveData);
--- src/modules/id.js
+++ src/modules/id.js
@@ -1,18 +1,18 @@
 // Create a new game
 const game = {
-    name: 'Tetris',
+  name: 'Tetris',
 };
 // function to get ID (only used once)
 const getID = async () => {
-    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
-        method: 'POST',
-        body: JSON.stringify(game),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    });
-    const APId = await response.json();
-    return APId;
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+    method: 'POST',
+    body: JSON.stringify(game),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const APId = await response.json();
+  return APId;
 };
 
 export default getID;
